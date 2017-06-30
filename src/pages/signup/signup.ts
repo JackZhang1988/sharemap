5e745b659a1afea86182b2ce8ae891f415beea96
zhangjianbo <zhangjianbo@weidian.com>
2017-06-30 10:03:25 +0800

    完成填写用户信息页面

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -3,9 +3,10 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 
+import 'web-animations-js/web-animations.min';
 import { MyValidator } from '../../common/validators';
 import { ApiService } from '../../services/api';
-import 'web-animations-js/web-animations.min';
+import { QiniuService } from '../../services/qiniu';
 /**
  * Generated class for the SignupPage page.
  *
@@ -16,7 +17,7 @@ import 'web-animations-js/web-animations.min';
 @Component({
   selector: 'page-signup',
   templateUrl: 'signup.html',
-  providers: [ApiService],
+  providers: [ApiService, QiniuService],
   animations: [
     trigger('enterTrigger', [
       state('swipRight', style({
@@ -32,15 +33,20 @@ import 'web-animations-js/web-animations.min';
 })
 export class SignupPage {
 
-  public signStep = 0;
-  public submitAttempt: boolean = false;
+  private signStep = 0;
+  private submitAttempt: boolean = false;
+  private stepTwoAttempt: boolean = false;
   public stepOneForm: FormGroup;
+  public stepTwoForm: FormGroup;
+  private imgLoading: boolean = false;
+  private avatar: string;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public formBuilder: FormBuilder,
     private apiService: ApiService,
+    private qiniuService: QiniuService,
     public toastCtrl: ToastController,
     public storage: Storage,
   ) {
@@ -51,6 +57,11 @@ export class SignupPage {
         password: ['', Validators.compose([Validators.required, Validators.minLength(5)])],
         passwordConfirm: ['', Validators.compose([Validators.required, Validators.minLength(5)])]
       }, { validator: MyValidator.passwordMatchValidator })
+    });
+    this.stepTwoForm = formBuilder.group({
+      userName: ['', Validators.required],
+      sex: ['', Validators.required],
+      signature: ['']
     });
   }
   ionViewDidLoad() {
@@ -70,12 +81,30 @@ export class SignupPage {
         msgCode: this.stepOneForm.controls.msgCode.value,
         password: this.stepOneForm.controls.passwords.value.password
       }
-      // this.apiService.signup(param).subscribe(res => {
-      //   if (res.status == 0) {
-      //     this.storage.set('userId', res.result.userId);
-      //     this.storage.set('token', res.result.token);
-      //   }
-      // })
+      this.apiService.signup(param).subscribe(res => {
+        if (res.status == 0) {
+          this.storage.set('userId', res.result.userId);
+          this.storage.set('token', res.result.token);
+        }
+      })
+    }
+  }
+
+  imgChange(event) {
+    if (!event.srcElement.files[0]) return;
+    this.imgLoading = true;
+    this.qiniuService.addImage(event.srcElement.files[0]).subscribe(imgUrl => {
+      if (imgUrl) {
+        this.imgLoading = false;
+        this.avatar = imgUrl;
+      }
+    })
+  }
+
+  updateUser() {
+    this.stepTwoAttempt = true;
+    if(this.stepTwoForm.valid){
+
     }
   }
 }
