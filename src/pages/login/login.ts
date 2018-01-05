2716fe203b56906228d76f7e144d428d321eb4a8
zhangjianbo <jackz001@yeah.net>
2018-01-05 15:03:32 +0800

    登录支持短信验证码登录

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,14 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { ApiService } from '../../services/api';
 import { Storage } from '@ionic/storage';
+import { Observable } from "rxjs";
 
-
-/**
- * Generated class for the LoginPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-login',
@@ -20,7 +14,11 @@ export class LoginPage {
 
   public phone: Number;
   public password: string;
+  public msgCode: string;
   public loginCallback: Function;
+  public loginType: string = 'password';
+  private sendMsgCodeBtnText: string = '发送验证码';
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
@@ -36,10 +34,12 @@ export class LoginPage {
   }
 
   login() {
-    if (this.phone && this.password) {
+    if (/1[0-9]{10}$/.test(this.phone.toString())) {
       this.apiService.login({
         phone: this.phone,
-        password: this.password
+        password: this.password,
+        msgCode: this.msgCode,
+        loginType: this.loginType
       }).subscribe(res => {
         if (res.status == 0) {
           this.storage.set('userId', res.result.userId);
@@ -47,12 +47,7 @@ export class LoginPage {
           if (res.result.signStatus == 1) {
             this.navCtrl.push('SignupPage', { signStep: 1 });
           } else {
-            let toast = this.toastCtrl.create({
-              message: '登录成功',
-              duration: 2000,
-              position: 'bottom'
-            })
-            toast.onDidDismiss(() => {
+            this.showMsg('登录成功', () => {
               this.navCtrl.setRoot('home');
               if (this.loginCallback) {
                 this.loginCallback();
@@ -60,17 +55,46 @@ export class LoginPage {
                 this.navCtrl.setRoot('home');
               }
             });
-            toast.present();
           }
         } else {
-          let toast = this.toastCtrl.create({
-            message: res.err,
-            duration: 2000,
-            position: 'bottom'
-          })
-          toast.present();
+          this.showMsg(res.err);
         }
       })
+    } else {
+      this.showMsg('请输入正确的手机号');
+    }
+  }
+
+  showMsg(msg, onDidDismiss?) {
+    let toast = this.toastCtrl.create({
+      message: msg,
+      duration: 2000,
+      position: 'bottom'
+    })
+    toast.present();
+    toast.onDidDismiss(() => {
+      onDidDismiss && onDidDismiss();
+    });
+  }
+
+  sendMsgCode() {
+    if (/1[0-9]{10}$/.test(this.phone.toString())) {
+      if (this.sendMsgCodeBtnText == '发送验证码') {
+        this.apiService.sendMsgCode(this.phone.toString()).subscribe(res => {
+          if (res.status == 0) {
+            let start = 60;
+            let timer = Observable.interval(1000).take(start);
+            timer.subscribe(x => {
+              this.sendMsgCodeBtnText = (--start) + '秒后重新发送';
+            })
+            timer.takeLast(1).subscribe(x => {
+              this.sendMsgCodeBtnText = '发送验证码';
+            });
+          }
+        });
+      }
+    } else {
+      this.showMsg('请输入正确的手机号');
     }
   }
 }
