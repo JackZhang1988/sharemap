8463d72989c06e27fcf83272ddd519e95814cb77
zhangjianbo <jackz001@yeah.net>
2018-02-25 14:35:21 +0800

    首页增加下拉刷新

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,21 +1,25 @@
-import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
+import { Component } from "@angular/core";
+import { OnInit } from "@angular/core";
 
-import { IonicPage, ModalController, NavController, FabContainer } from 'ionic-angular';
-import { Map } from '../../common/models';
-import { ApiService } from '../../services/api';
-import { AuthServiceProvider } from '../../providers/auth';
+import {
+  IonicPage,
+  ModalController,
+  NavController,
+  FabContainer
+} from "ionic-angular";
+import { Map } from "../../common/models";
+import { ApiService } from "../../services/api";
+import { AuthServiceProvider } from "../../providers/auth";
 
 @IonicPage({
-  name: 'home'
+  name: "home"
 })
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html',
+  selector: "page-home",
+  templateUrl: "home.html",
   providers: [ApiService, AuthServiceProvider, FabContainer]
 })
 export class HomePage implements OnInit {
-
   maps: Map[] = [];
   hasMore: boolean = true;
   pageSize: Number = 10;
@@ -24,78 +28,92 @@ export class HomePage implements OnInit {
     public modalCtrl: ModalController,
     public navCtrl: NavController,
     private authService: AuthServiceProvider,
-    private apiService: ApiService) { }
+    private apiService: ApiService
+  ) {}
 
   ngOnInit(): void {
     this.getMaps({ pageSize: this.pageSize });
   }
-  getMaps(param?, callback?): void {
+  getMaps(param?, callback?, isRefresh = false): void {
     this.apiService.getMaps(param).subscribe(res => {
       if (res.status == 0) {
-        this.maps = this.maps.concat(res.result);
+        this.maps = isRefresh ? res.result : this.maps.concat(res.result);
         if (res.result.length < this.pageSize) {
           this.hasMore = false;
-          callback && callback()
+          callback && callback();
         }
       }
     });
   }
 
   goContactPage() {
-    this.authService.checkLogin().then(token => {
-      this.navCtrl.push('ProfilePage');
-    }, () => {
-      this.navCtrl.push('LoginPage', {
-        callback: () => {
-          this.navCtrl.push('ProfilePage')
-        }
-      });
-    })
+    this.authService.checkLogin().then(
+      token => {
+        this.navCtrl.push("ProfilePage");
+      },
+      () => {
+        this.navCtrl.push("LoginPage", {
+          callback: () => {
+            this.navCtrl.push("ProfilePage");
+          }
+        });
+      }
+    );
   }
 
   goMapDetail(mapData) {
-    this.navCtrl.push('MapDetailPage', {
+    this.navCtrl.push("MapDetailPage", {
       id: mapData._id
-    })
+    });
   }
 
   openAddModal(type, fab: FabContainer) {
     fab.close();
-    this.authService.checkLogin().then(token => {
-      let curModal;
-      if (type == 'map') {
-        curModal = this.modalCtrl.create('AddMapModal');
-      } else {
-        curModal = this.modalCtrl.create('SearchLocModal');
-      }
-      curModal.onDidDismiss(callbackData=>{
-        if(callbackData && callbackData.status == 'add-map-done'){
-          //表示添加地图集成功
-          this.goMapDetail(callbackData.data);
-        }else if(callbackData && callbackData.status == 'add-location-done'){
-          //添加地点成功
-          this.navCtrl.push("LocationDetailPage", {
-            id: callbackData.data._id
-          });
+    this.authService.checkLogin().then(
+      token => {
+        let curModal;
+        if (type == "map") {
+          curModal = this.modalCtrl.create("AddMapModal");
+        } else {
+          curModal = this.modalCtrl.create("SearchLocModal");
         }
-      })
-      curModal.present();
-
-    }, () => {
-      this.navCtrl.push('LoginPage');
-    })
+        curModal.onDidDismiss(callbackData => {
+          if (callbackData && callbackData.status == "add-map-done") {
+            //表示添加地图集成功
+            this.goMapDetail(callbackData.data);
+          } else if (
+            callbackData &&
+            callbackData.status == "add-location-done"
+          ) {
+            //添加地点成功
+            this.navCtrl.push("LocationDetailPage", {
+              id: callbackData.data._id
+            });
+          }
+        });
+        curModal.present();
+      },
+      () => {
+        this.navCtrl.push("LoginPage");
+      }
+    );
   }
 
   goSearchPage(e) {
     e.stopPropagation();
-    this.navCtrl.push('SearchPage');
+    this.navCtrl.push("SearchPage");
   }
 
   doInfinite(infiniteScroll) {
-    console.log('Begin async operation');
+    console.log("Begin async operation");
     this.getMaps({
       pageSize: this.pageSize,
       lastItemDate: this.maps[this.maps.length - 1].addDate
-    })
+    });
+  }
+  doRefresh(refresher) {
+    this.getMaps({ pageSize: this.pageSize }, () => {
+      refresher.complete();
+    }, true);
   }
 }
