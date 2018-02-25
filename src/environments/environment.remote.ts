fa18073b85678797acb60fa0ec96fe2d03d4cfcf
zhangjianbo <jackz001@yeah.net>
2018-02-25 13:50:22 +0800

    bug fix: 添加地图封面选择图片可以多选，应该是单选 Header status bar bug fix

--- src/environments/environment.remote.ts
+++ src/environments/environment.remote.ts
@@ -2,6 +2,6 @@ import { Environment } from './environment.model';
 
 export const ENV: Environment = {
   mode: 'Production',
-  API_URL: 'http://172.16.193.104:3000',
+  API_URL: 'http://192.168.1.7:3000',
   SHARE_BASE_URL:'http://www.fenxiangditu.com/mobile/'
 }
