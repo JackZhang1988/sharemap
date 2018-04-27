6433c8f829fe037db4a76853738ba337d3cbce74
zhangjianbo <jackz001@yeah.net>
2018-04-27 12:00:22 +0800

    地图点击海量点显示详情slide

--- src/pages/modals/map-view.ts
+++ src/pages/modals/map-view.ts
@@ -47,6 +47,8 @@ export class MapViewModal {
     private pathSearchTrigger: boolean = false;
     private showPathPanel: boolean = false;
     private isMass: boolean = false;
+    private showSlides: boolean = false;
+    private slidesData: any[]= [];
 
     //使用动态id，方式生成多个modal时id重复
     private randomMapId: string = 'mapContainer-' + new Date().getTime().toString(32);
@@ -64,7 +66,8 @@ export class MapViewModal {
             {
                 panToLocation: false,
                 zoomToAccuracy: false,
-                buttonOffset: buttonOffset
+                buttonPosition: 'LT',
+                buttonOffset: new AMap.Pixel(10, 10)
             },
             result => {
                 this.curPosition = result;
@@ -94,12 +97,17 @@ export class MapViewModal {
                             };
                         });
                         //数量庞大的marker使用massMarks
-                        this.gdService.addMassMarks(markList);
-
+                        this.gdService.addMassMarks(markList, {
+                            markerClick: locData => {
+                                this.slidesData = [locData.info];
+                            }
+                        });
+                        
                         // this.gdService.addMarkerClusterer(markList)
                         // let mock =  [{"lnglat":[116.258446,37.686622],"name":"景县","style":2},{"lnglat":[113.559954,22.124049],"name":"圣方济各堂区","style":2},{"lnglat":[116.366794,39.915309],"name":"西城区","style":2},{"lnglat":[116.486409,39.921489],"name":"朝阳区","style":2},{"lnglat":[116.286968,39.863642],"name":"丰台区","style":2},{"lnglat":[116.195445,39.914601],"name":"石景山区","style":2},{"lnglat":[116.310316,39.956074],"name":"海淀区","style":2},{"lnglat":[116.105381,39.937183],"name":"门头沟区","style":2}];
                         // this.gdService.addMassMarks(mock);
                     } else {
+                        this.slidesData = this.mapLocations;
                         markList = res.result.locations.map(item => {
                             return item.lnglat;
                         });
@@ -111,9 +119,12 @@ export class MapViewModal {
                             this.gdService.setFitView();
                         });
                     }
+                    // this.initSlideWidth();
                 }
             });
         } else {
+            this.slidesData = this.mapLocations;
+            // this.initSlideWidth();
             for (let item of this.mapLocations) {
                 markList.push(item.lnglat);
             }
@@ -125,11 +136,8 @@ export class MapViewModal {
                 this.gdService.setFitView();
             });
         }
-    }
-
-    ngAfterViewInit() {
         if (this.type == 'map-locations') {
-            this.slides.width = window.screen.width * 0.8;
+            this.slides.width = window.screen.width * 0.95;
         }
     }
 
