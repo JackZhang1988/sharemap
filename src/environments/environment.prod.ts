1d526238345eb2ba563a0c3b6668de4cf3f6f596
zhangjianbo <jackz001@yeah.net>
2018-05-10 11:44:23 +0800

    app  rename to 人人地图

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,6 @@ import { Environment } from './environment.model';
 
 export const ENV: Environment = {
   mode: 'Production',
-  API_URL: 'http://fenxiangditu.com',
-  // SHARE_BASE_URL:'http://www.fenxiangditu.com/mobile/'
-  SHARE_BASE_URL:'http://fenxiangditu.com/'
+  API_URL: 'http://renrenmap.cn',
+  SHARE_BASE_URL:'http://renrenmap.cn/'
 }
--- src/environments/environment.remote.ts
+++ src/environments/environment.remote.ts
@@ -3,5 +3,5 @@ import { Environment } from './environment.model';
 export const ENV: Environment = {
   mode: 'Production',
   API_URL: 'http://192.168.1.7:3000',
-  SHARE_BASE_URL:'http://www.fenxiangditu.com/mobile/'
+  SHARE_BASE_URL:'http://www.renrenmap.cn/mobile/'
 }
