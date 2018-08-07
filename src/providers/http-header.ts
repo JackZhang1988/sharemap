1bffb94240ff6604651ecee076ac058774291df6
zhangjianbo <jackz001@yeah.net>
2018-08-07 09:39:05 +0800

    chore(http-header): remove code

--- src/providers/http-header.ts
+++ src/providers/http-header.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
-
 import { Observable } from 'rxjs/Observable';
-
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthServiceProvider } from './auth';
 
 /** Pass untouched request through to the next request handler. */
@@ -12,16 +9,6 @@ export class HttpHeaderInterceptor implements HttpInterceptor {
     constructor(private auth:AuthServiceProvider){}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        // let storage = new Storage({});
-        // return next.handle(req);
-        // storage.get('userId').then(userId => {
-        //     if(userId){
-        //         const modified = req.clone({ setHeaders: { 'X-User-Id': userId } });
-        //         return next.handle(modified);
-        //     }else{
-        //         return next.handle(req);
-        //     }
-        // });
         return Observable.fromPromise(this.handleAccess(req, next));
     }
     private async handleAccess(req: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
